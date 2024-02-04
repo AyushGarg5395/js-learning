01b36b28c2269220d7dabe456d4806b76fc63560
Ayush Garg <ytayushgarg@gmail.com>
2024-02-04 14:49:05 +0530

    adding files of day 2

---
+++ 01_variables.js
@@ -0,0 +1,14 @@
+const accountId= 14332
+let accountEmail= "saiuohf@gmail.com"
+var accountPassword= "1234"
+accountCity= "jaipur"
+
+// accountId= 2 // Not Allowed 
+
+accountEmail= "asno@gmail.com"
+accountPassword= "124"
+accountCity= "BEngluru"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountCity,accountPassword])
